5f3bcd2853c76c8091b3df5b24b087258705f0f2
Keith Batten <kbatten@gmail.com>
2022-03-12 16:40:55 -0800

    Add multiple thrust styles
    fix issue where thrust wasn't following the player direction

--- ActionBasedThrusterProvider.cs
+++ ActionBasedThrusterProvider.cs
@@ -12,14 +12,39 @@ namespace UnityEngine.XR.Interaction.Toolkit
     [AddComponentMenu("XR/Locomotion/Thruster Provider (Action-based)", 11)]
     public class ActionBasedThrusterProvider : MonoBehaviour
     {
+        public enum thrusterStyle
+        {
+            normal,
+            superman,
+            ironman
+        }
+
+        [SerializeField]
+        public GameObject m_Player;
+
         [SerializeField]
         [Tooltip("The Input System Action that will be used to read Thruster data from the controller. Must be a Value Touch Control.")]
         List<InputActionProperty> m_ThrusterActions;
 
         [SerializeField]
-        public float m_ThrusterMultipier = 10.0f;
+        [Tooltip("The Input System Action that will be used to read rotation data from the controller. Must be a Value Quaternion.")]
+        InputActionProperty m_ThrusterDirectionAction;
 
+        private GameObject m_ThrusterDirection;
+
+        [SerializeField]
+        [Tooltip("The Input System Action that will be used to read button data from the controller. Must be a Button Control.")]
+        List<InputActionProperty> m_ThrusterStyleActions;
+
+        [SerializeField]
+        public thrusterStyle m_ThrusterStyle = thrusterStyle.normal;
+
+        [SerializeField]
+        public float m_ThrusterMultiplier = 1.0f;
+
+        private float m_ThrusterInternalMultiplier = 20.0f;
         private ConstantForce m_Thruster;
+
 
         protected void OnEnable()
         {
@@ -27,10 +52,22 @@ namespace UnityEngine.XR.Interaction.Toolkit
             {
                 thrusterAction.EnableDirectAction();
 
-                thrusterAction.action.started += ThrusterStart;
                 thrusterAction.action.performed += ThrusterUpdate;
                 thrusterAction.action.canceled += ThrusterEnd;
             }
+
+            m_ThrusterDirectionAction.EnableDirectAction();
+            m_ThrusterDirectionAction.action.performed += ThrusterDirectionUpdate;
+            m_ThrusterDirectionAction.action.performed += ThrusterUpdate;
+            if (m_ThrusterDirection == null) m_ThrusterDirection = new GameObject();
+
+            foreach (InputActionProperty thrusterStyleAction in m_ThrusterStyleActions)
+            {
+                thrusterStyleAction.EnableDirectAction();
+
+                thrusterStyleAction.action.started += ThrusterStyleStart;
+            }
+
             if (m_Thruster == null) m_Thruster = gameObject.AddComponent<ConstantForce>();
         }
 
@@ -38,27 +75,70 @@ namespace UnityEngine.XR.Interaction.Toolkit
         {
             foreach (InputActionProperty thrusterAction in m_ThrusterActions)
             {
-                thrusterAction.action.started -= ThrusterStart;
                 thrusterAction.action.performed -= ThrusterUpdate;
                 thrusterAction.action.canceled -= ThrusterEnd;
 
                 thrusterAction.DisableDirectAction();
             }
-        }
 
-        private void ThrusterStart(InputAction.CallbackContext obj)
-        {
+            m_ThrusterDirectionAction.action.performed -= ThrusterUpdate;
+            m_ThrusterDirectionAction.action.performed -= ThrusterDirectionUpdate;
+            m_ThrusterDirectionAction.DisableDirectAction();
+
+            foreach (InputActionProperty thrusterStyleAction in m_ThrusterStyleActions)
+            {
+                thrusterStyleAction.EnableDirectAction();
+
+                thrusterStyleAction.action.started -= ThrusterStyleStart;
+            }
         }
 
         private void ThrusterUpdate(InputAction.CallbackContext obj)
         {
-            var value = m_ThrusterMultipier * Mathf.Sqrt(ReadValue());
-            m_Thruster.force = new Vector3(0.0f, value, 0.0f);
+            Vector3 direction = m_ThrusterDirection.transform.up;
+            switch (m_ThrusterStyle) {
+                case thrusterStyle.normal:
+                    direction = m_Player.transform.rotation * m_ThrusterDirection.transform.up;
+                    break;
+                case thrusterStyle.superman:
+                    direction = m_Player.transform.rotation * m_ThrusterDirection.transform.forward;
+                    break;
+                case thrusterStyle.ironman:
+                    direction = m_Player.transform.rotation * m_ThrusterDirection.transform.forward * -1;
+                    break;
+            }
+            m_Thruster.force = direction * m_ThrusterInternalMultiplier * m_ThrusterMultiplier * Mathf.Sqrt(ReadValue());
         }
 
         private void ThrusterEnd(InputAction.CallbackContext obj)
         {
             m_Thruster.force = Vector3.zero;
+        }
+
+        private void ThrusterDirectionUpdate(InputAction.CallbackContext obj)
+        {
+            m_ThrusterDirection.transform.rotation = ReadDirectionValue();
+        }
+
+        private void ThrusterStyleStart(InputAction.CallbackContext obj)
+        {
+            switch (m_ThrusterStyle)
+            {
+                case thrusterStyle.normal:
+                    m_ThrusterStyle = thrusterStyle.superman;
+                    break;
+                case thrusterStyle.superman:
+                    m_ThrusterStyle = thrusterStyle.ironman;
+                    break;
+                case thrusterStyle.ironman:
+                    m_ThrusterStyle = thrusterStyle.normal;
+                    break;
+            }
+        }
+
+        protected Quaternion ReadDirectionValue()
+        {
+            return  m_ThrusterDirectionAction.action?.ReadValue<Quaternion>() ?? Quaternion.identity;
         }
 
         protected float ReadValue()
