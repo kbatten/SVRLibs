0c1483872fbcb1c8df698a84b084dd5545f7f5ff
Keith Batten <kbatten@gmail.com>
2022-07-26 18:33:02 -0700

    WIP automatically get teleport thresholds

--- ActionBasedTeleportationProvider.cs
+++ ActionBasedTeleportationProvider.cs
@@ -1,6 +1,7 @@
 using UnityEngine.InputSystem;
-using UnityEngine.XR.Interaction.Toolkit.Inputs;
+using UnityEngine.XR.Interaction.Toolkit.Inputs.Interactions;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace UnityEngine.XR.Interaction.Toolkit
 {
@@ -28,16 +29,42 @@ namespace UnityEngine.XR.Interaction.Toolkit
         private bool m_ReadyToTeleport = false;
         private float m_OriginalLineWidth;
 
-        private float m_ViewThreshold = 0.5f; // FIX: this must match with Press Point
-        private float m_Threshold = 0.75f; // FIX: this must match with Press Point
+        private float m_ViewThreshold = 0.0f;
+        private float m_ActivateThreshold = 1.0f;
 
         protected void OnEnable()
         {
             m_OriginalLineWidth = m_RayVisual?.lineWidth ?? 0.0f;
+
+            m_ViewThreshold = 0.0f;
+            bool updatedViewThreshold = false;
+            m_ActivateThreshold = 1.0f;
+            bool updatedActivateThreshold = false;
+
             foreach (InputActionReference teleportAction in m_TeleportActions)
             {
-                Debug.Log($"1 {teleportAction.action.bindingMask.ToString()}");
-                Debug.Log($"2 {teleportAction.action.bindings.ToString()}");
+                // Supports Vector2 Actions with an optional shared pair of SectorInteraction pressPoints
+                foreach (InputBinding inputBinding in teleportAction.action.bindings)
+                {
+                    foreach (SectorInteraction interaction in ParseSectorInteractions(inputBinding.interactions))
+                    {
+                        if (!updatedViewThreshold)
+                        {
+                            m_ViewThreshold = 1.0f;
+                            updatedViewThreshold = true;
+                        }
+                        if (!updatedActivateThreshold)
+                        {
+                            m_ActivateThreshold = 0.0f;
+                            updatedActivateThreshold = true;
+                        }
+                        // lowest value of pressPoint is view threshold
+                        if (interaction.pressPoint < m_ViewThreshold) m_ViewThreshold = interaction.pressPoint;
+                        // highest value of pressPoint is activate threshold
+                        if (interaction.pressPoint > m_ActivateThreshold) m_ActivateThreshold = interaction.pressPoint;
+                    }
+                }
+                Debug.Log($"{m_ViewThreshold} {m_ActivateThreshold}");
                 //teleportAction.EnableDirectAction();
 
                 teleportAction.action.started += TeleportStart;
@@ -73,11 +100,11 @@ namespace UnityEngine.XR.Interaction.Toolkit
         {
             var value = ReadValue(obj);
             Debug.Log($"TeleportUpdate {value} {obj.interaction}");
-            if (!m_ReadyToTeleport && value < m_Threshold)
+            if (!m_ReadyToTeleport && value < m_ActivateThreshold)
             {
-                if (m_RayVisual != null) m_RayVisual.lineWidth = (1.0f - ((m_Threshold - value) / (m_Threshold - m_ViewThreshold))) * m_OriginalLineWidth;
+                if (m_RayVisual != null) m_RayVisual.lineWidth = (1.0f - ((m_ActivateThreshold - value) / (m_ActivateThreshold - m_ViewThreshold))) * m_OriginalLineWidth;
             }
-            else if (!m_ReadyToTeleport && value >= m_Threshold)
+            else if (!m_ReadyToTeleport && value >= m_ActivateThreshold)
             {
                 m_ReadyToTeleport = true;
                 if (m_RayVisual != null) m_RayVisual.lineWidth = m_OriginalLineWidth;
@@ -119,6 +146,31 @@ namespace UnityEngine.XR.Interaction.Toolkit
                 r += y;
             }*/
             return r;
+        }
+
+        // Look for SectorInteractions in a string
+        protected List<SectorInteraction> ParseSectorInteractions(string interactions)
+        {
+            var ret = new List<SectorInteraction>();
+
+            Regex rSector = new Regex(@"Sector\([^)]*\)", RegexOptions.IgnoreCase);
+            Regex rPressPoint = new Regex(@"pressPoint=[0-9-.]*", RegexOptions.IgnoreCase);
+
+            float pressPoint;
+
+            foreach (Match m in rSector.Matches(interactions))
+            {
+                pressPoint = SectorInteraction.defaultPressPoint;
+                Match mPresspoint = rPressPoint.Match(m.Value);
+                if (mPresspoint.Success)
+                {
+                    pressPoint = float.Parse(mPresspoint.Value.Split("=")[1], System.Globalization.CultureInfo.InvariantCulture);
+                }
+                var s = new SectorInteraction();
+                s.pressPoint = pressPoint;
+                ret.Add(s);
+            }
+            return ret;
         }
     }
 }
